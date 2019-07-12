eb080695d8f157960c42a06479d6fc8536ce6a3f
Krzysztof Nowicki <krzysztof11nowicki@gmail.com>
2019-07-12 23:54:51 +0200

    first phaser game - tutorial

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import "./main.scss"; // important for build - do not remove
+import { importTest } from "./js/someJSModule"; // import example
+
+console.log(importTest);
+console.log("dupa");
+
+
---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+/*******
+ * config for React and Vue here: https://www.valentinog.com/blog/webpack-tutorial/
+ */
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          "style-loader",
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              url: false,
+            },
+          },
+          "sass-loader"
+        ]
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+            options: { minimize: true }
+          }
+        ]
+      }     
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    })
+  ]
+};
